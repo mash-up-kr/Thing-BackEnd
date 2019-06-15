5c06056514770171fae8387702ea14fbc1e8f021
korea8378 <korea8378@naver.com>
2019-06-15 15:38:48 +0900

    [MASTER][ADD] init

---
+++ src/main/java/com/mashup/thing/ThingApplication.java
@@ -0,0 +1,13 @@
+package com.mashup.thing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThingApplication.class, args);
+    }
+
+}
